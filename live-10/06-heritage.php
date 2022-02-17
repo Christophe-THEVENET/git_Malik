29805183a5cf71a6ee93583cc4f1172abe2ae907
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-02-17 15:55:50 +0100

    top
    Signed-off-by: Christophe THEVENET <christophethevenet2.0@gmail.com>

---
+++ live-10/06-heritage.php
@@ -0,0 +1,38 @@
+<?php 
+
+class A {
+    public $url = "http://yahoo.fr";
+    public function a(){}
+}
+
+class B{
+    public $url = "http://yahoo.fr";
+    public function b(){}
+}
+
+//héritage copier méthode / propriété entre un class parent et ses class enfant (hérité)
+
+class CParent{
+    protected $url = "http://studi.fr";
+}
+
+class AEnfant extends CParent{
+    public $url = "toto"; 
+    public function a(){}
+}
+
+class BEnfant extends CParent{
+    public function b(){}
+}
+
+$a = new AEnfant();
+
+var_dump($a->url); 
+$a->a();
+
+$b = new BEnfant();
+var_dump($b->url);
+$b->b();
+
+// $b->a(); // erreur
+// Fatal error: Uncaught Error: Call to undefined method BEnfant::a()
